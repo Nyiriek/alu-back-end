85a23a60b9103ca99fc5b2cbb2bfcdc72d1cc38e
Nyiriek <n.peat@alustudent.com>
2023-07-09 20:34:40 +0000

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""A script that exports data in csv format"""
-
+""" Call API and store data in CSV """
 import csv
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    url_todo = f'https://jsonplaceholder.typicode.com/users/{employeeId}/todos/'
-    url_user = 'https://jsonplaceholder.typicode.com/users'
-    todo = requests.get(url_todo, params={'userId': employeeId})
-    user = requests.get(url_user, params={'id': employeeId})
+    employee_id = argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'userId': employee_id})
+    user = requests.get(url_user, params={'id': employee_id})
 
-    todo_data = todo.json()
-    user_data = user.json()
+    todo_dict_list = todo.json()
+    user_dict_list = user.json()
 
-    completed_tasks = []
-    employee_name = user_data[0].get('name')
+    employee = user_dict_list[0].get('username')
 
-    for task in todo_data:
-        if task['completed']:
-            completed_tasks.append(task)
-
-    csv_file_name = f'{employeeId}.csv'
-
-    with open(csv_file_name, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in completed_tasks:
-            writer.writerow([employeeId, employee_name, "Completed", task.get('title')])
-
-    print(f"Data exported to {csv_file_name} successfully.")
+    with open("{}.csv".format(employee_id), "a+") as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo_dict_list:
+            status = task['completed']
+            title = task['title']
+            csvwriter.writerow(["{}".format(employee_id),
+                                "{}".format(employee),
+                                "{}".format(status),
+                                "{}".format(title)])
